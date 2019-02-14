5bdd86c1007c12544eab41ee44209d8c2c743929
Sofia Bariami <44237397+SofiaBariami@users.noreply.github.com>
2019-02-14 00:23:07 +0000

    Read xml file, store molecule parameters

--- readxml/readxml.py
+++ readxml/readxml.py
@@ -17,7 +17,526 @@ if __name__ == '__main__':
     molecules = s.molecules()
     print (molecules)
     # 2) Now we read the xml file, and store parameters for each molecule
-    # TODO by Sofia
+
+
+    import xml.dom.minidom as minidom
+    xmldoc = minidom.parse('pyridine/MOL_extra.xml')
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~ TAG NAME: ATOM ~~~~~~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    itemlist_atom = xmldoc.getElementsByTagName('Atom')
+    dicts_atom = []
+    for items in itemlist_atom:
+        d = {}
+        for a in items.attributes.values():
+            d[a.name] = a.value
+        dicts_atom.append(d)
+    dicts_at =  str(dicts_atom).split()
+    #print (dicts_at)
+
+    nameList =[]
+    for i in range (len(dicts_at)-1):
+        name = {}
+        if dicts_at[i-1] == "{'name':" or dicts_at[i-1] == "[{'name':":
+            name= dicts_at[i].replace("'," , " ").replace("," , " ").replace("}" , " ").replace("'" , " ")
+            nameList.append(name)
+    print ("The name list is:")
+    print (nameList)
+
+    typeList =[]
+    for i in range (len(dicts_at)-1):
+        t = {}
+        if dicts_at[i-1] == "{'type':":
+            t= dicts_at[i].replace("'," , " ").replace("," , " ").replace("}" , " ").replace("'" , " ")
+            typeList.append(t)
+    print ("The type list is:")
+    print (typeList)
+
+
+    chargeList =[]
+    for i in range (len(dicts_at)-1):
+        t = {}
+        if dicts_at[i-1] == "'charge':":
+            t= float(dicts_at[i].replace("'," , " ").replace("," , " ").replace("}" , " ").replace("'" , " "))
+            chargeList.append(t)
+    print ("The charge list is:")
+    print (chargeList)
+
+    sigmaList =[]
+    for i in range (len(dicts_at)-1):
+        s = {}
+        if dicts_at[i-1] == "'sigma':":
+            s= float(dicts_at[i].replace("'," , " ").replace("," , " ").replace("}" , " ").replace("'" , " "))
+            sigmaList.append(s)
+    print ("The sigma list is:")
+    print (sigmaList)
+
+
+    epsilonList =[]
+    for i in range (len(dicts_at)-1):
+        e = {}
+        if dicts_at[i-1] == "'epsilon':":
+            e= float(dicts_at[i].replace("'," , " ").replace("," , " ").replace("}" , " ").replace("'" , " "))
+            epsilonList.append(e)
+    print ("The epsilon list is:")
+    print (epsilonList)
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~ TAG NAME: BOND ~~~~~~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    itemlist_bond = xmldoc.getElementsByTagName('Bond')
+    dicts_bond = []
+    for items in itemlist_bond:
+        d = {}
+        for a in items.attributes.values():
+            d[a.name] = a.value
+        dicts_bond.append(d)
+    dicts_b =  str(dicts_bond).split()
+    #print (dicts_b)
+
+    fromList =[]
+    for i in range (len(dicts_b)-1):
+        f = {}
+        if dicts_b[i-1] == "{'from':" or dicts_b[i-1] == "[{'from':":
+            f= int(dicts_b[i].replace("'," , " ").replace("," , " ").replace("}" , " ").replace("'" , " "))
+            fromList.append(f)
+    print ("The from list is:")
+    print (fromList)
+
+    toList =[]
+    for i in range (len(dicts_b)-1):
+        t = {}
+        if dicts_b[i-1] == "'to':":
+            t= int(dicts_b[i].replace("'," , " ").replace("," , " ").replace("}" , " ").replace("'" , " "))
+            toList.append(t)
+    print ("The to list is:")
+    print (toList)
+
+    lengthList =[]
+    for i in range (len(dicts_b)-1):
+        l = {}
+        if dicts_b[i-1] == "'length':":
+            l= float(dicts_b[i].replace("'," , " ").replace("," , " ").replace("}" , " ").replace("'" , " "))
+            lengthList.append(l)
+    print ("The length list is:")
+    print (lengthList)
+
+    k_bondList =[]
+    for i in range (len(dicts_b)-1):
+        l = {}
+        if dicts_b[i-1] == "'k':":
+            l= float(dicts_b[i].replace("'," , " ").replace("," , " ").replace("}" , " ").replace("'" , " "))
+            k_bondList.append(l)
+    print ("The k_bond list is:")
+    print (k_bondList)
+
+    cl1List =[]
+    for i in range (len(dicts_b)-1):
+        l = {}
+        if dicts_b[i-1] == "{'class1':"or dicts_b[i-1] == "[{'class1':":
+            l= dicts_b[i].replace("'," , " ").replace("," , " ").replace("}" , " ").replace("'" , " ")
+            cl1List.append(l)
+    print ("The class1 list is:")
+    print (cl1List)
+
+    cl2List =[]
+    for i in range (len(dicts_b)-1):
+        l = {}
+        if dicts_b[i-1] == "'class2':":
+            l= dicts_b[i].replace("'," , " ").replace("," , " ").replace("}" , " ").replace("'" , " ")
+            cl2List.append(l)
+    print ("The class2 list is:")
+    print (cl2List)   
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~ TAG NAME: ANGLE ~~~~~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    itemlist_angle = xmldoc.getElementsByTagName('Angle')
+    dicts_angle = []
+    for items in itemlist_angle:
+        d = {}
+        for a in items.attributes.values():
+            d[a.name] = a.value
+        dicts_angle.append(d)
+    dicts_ang =  str(dicts_angle).split()
+    #print (dicts_ang)
+
+
+
+    cl1_angle_List =[]
+    for i in range ( len(dicts_ang)-1):
+        l = {}
+        if dicts_ang[i] == "{'class1':" or dicts_ang[i] == "[{'class1':":
+            l= dicts_ang[i+1].replace("'," , " ").replace("," , " ").replace("}" , " ").replace("'" , " ")
+            cl1_angle_List.append(l)
+    print ("The class1 angle list is:")
+    print (cl1_angle_List)
+
+
+
+    dct = {}
+    for k in range(2,4):
+        dct["cl%s" %k] = []
+        for i in range (len(dicts_ang)-1):
+            l = {}
+            if dicts_ang[i] == "'class%s"%k+"':":
+                d={}
+                l= dicts_ang[i+1].replace("'," , " ").replace("," , " ").replace("}" , " ").replace("'" , " ")
+                dct["cl"+str(k)].append(l)
+        print ("The class%s "%k+"angle list is:")
+        print (dct["cl"+str(k)])
+
+
+    angle_List =[]
+    for i in range (len(dicts_ang)-1):
+        l = {}
+        if dicts_ang[i] == "'angle':":
+            l= float(dicts_ang[i+1].replace("'," , " ").replace("," , " ").replace("}" , " ").replace("'" , " "))
+            angle_List.append(l)
+    print ("The angle list is:")
+    print (angle_List)
+
+
+    k_angle_List =[]
+    for i in range (len(dicts_ang)-1):
+        l = {}
+        if dicts_ang[i] == "'k':":
+            l= float(dicts_ang[i+1].replace("'}]" , " ").replace("'," , " ").replace("," , " ").replace("}" , " ").replace("'" , " "))
+            k_angle_List.append(l)
+    print ("The k angle list is:")
+    print (k_angle_List)
+
+
+
+
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~ TAG NAME: PROPER ~~~~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    itemlist_proper = xmldoc.getElementsByTagName('Proper')
+    dicts_proper = []
+    for items in itemlist_proper:
+        d = {}
+        for a in items.attributes.values():
+            d[a.name] = a.value
+        dicts_proper.append(d)
+    dicts_pr =  str(dicts_proper).split()
+    #print (dicts_pr)
+
+
+
+
+    cl1_proper_List =[]
+    for i in range ( len(dicts_pr)-1):
+        l = {}
+        if dicts_pr[i] == "{'class1':" or dicts_pr[i] == "[{'class1':":
+            l= dicts_pr[i+1].replace("'," , " ").replace("," , " ").replace("}" , " ").replace("'" , " ")
+            cl1_proper_List.append(l)
+    print ("The class1 proper list is:")
+    print (cl1_proper_List)
+
+
+
+    dct = {}
+    for k in range(2,5):
+        dct["cl%s" %k] = []
+        for i in range (len(dicts_pr)-1):
+            l = {}
+            if dicts_pr[i] == "'class%s"%k+"':":
+                d={}
+                l= dicts_pr[i+1].replace("'," , " ").replace("," , " ").replace("}" , " ").replace("'" , " ")
+                dct["cl"+str(k)].append(l)
+        print ("The class%s "%k+"proper list is:")
+        print (dct["cl"+str(k)])
+
+
+
+    dct = {}
+    for k in range(1,5):
+        dct["k%s" %k] = []
+        for i in range (len(dicts_pr)-1):
+            l = {}
+            if dicts_pr[i] == "'k%s"%k+"':":
+                d={}
+                l= float(dicts_pr[i+1].replace("'," , " ").replace("," , " ").replace("}" , " ").replace("'" , " "))
+                dct["k"+str(k)].append(l)
+        print ("The k%s "%k+"proper list is:")
+        print (dct["k"+str(k)])
+
+
+    dct = {}
+    for k in range(1,5):
+        dct["periodicity%s" %k] = []
+        for i in range (len(dicts_pr)-1):
+            l = {}
+            if dicts_pr[i] == "'periodicity%s"%k+"':":
+                d={}
+                l= int(dicts_pr[i+1].replace("'," , " ").replace("," , " ").replace("}" , " ").replace("'" , " "))
+                dct["periodicity"+str(k)].append(l)
+        print ("The periodicity%s "%k+"proper list is:")
+        print (dct["periodicity"+str(k)])
+
+
+
+    dct = {}
+    for k in range(1,5):
+        dct["phase%s" %k] = []
+        for i in range (len(dicts_pr)-1):
+            l = {}
+            if dicts_pr[i] == "'phase%s"%k+"':":
+                d={}
+                l= float(dicts_pr[i+1].replace("'," , " ").replace("," , " ").replace("}" , " ").replace("'" , " ").replace("]", " "))
+                dct["phase"+str(k)].append(l)
+        print ("The phase%s "%k+"proper list is:")
+        print (dct["phase"+str(k)])
+
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~ TAG NAME: IMPROPER ~~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    itemlist_improper = xmldoc.getElementsByTagName('Improper')
+    dicts_improper = []
+    for items in itemlist_improper:
+        d = {}
+        for a in items.attributes.values():
+            d[a.name] = a.value
+        dicts_improper.append(d)
+    dicts_impr =  str(dicts_improper).split()
+    #print (dicts_impr)
+
+
+
+    cl1_improper_List =[]
+    for i in range ( len(dicts_impr)-1):
+        l = {}
+        if dicts_impr[i] == "{'class1':" or dicts_impr[i] == "[{'class1':":
+            l= dicts_impr[i+1].replace("'," , " ").replace("," , " ").replace("}" , " ").replace("'" , " ")
+            cl1_improper_List.append(l)
+    print ("The class1 improper list is:")
+    print (cl1_improper_List)
+
+
+
+    dct = {}
+    for k in range(2,5):
+        dct["cl%s" %k] = []
+        for i in range (len(dicts_impr)-1):
+            l = {}
+            if dicts_impr[i] == "'class%s"%k+"':":
+                d={}
+                l= dicts_impr[i+1].replace("'," , " ").replace("," , " ").replace("}" , " ").replace("'" , " ")
+                dct["cl"+str(k)].append(l)
+        print ("The class%s "%k+"improper list is:")
+        print (dct["cl"+str(k)])
+
+
+
+    dct = {}
+    for k in range(1,5):
+        dct["k%s" %k] = []
+        for i in range (len(dicts_impr)-1):
+            l = {}
+            if dicts_impr[i] == "'k%s"%k+"':":
+                d={}
+                l= float(dicts_impr[i+1].replace("'," , " ").replace("," , " ").replace("}" , " ").replace("'" , " "))
+                dct["k"+str(k)].append(l)
+        print ("The k%s "%k+"improper list is:")
+        print (dct["k"+str(k)])
+
+
+    dct = {}
+    for k in range(1,5):
+        dct["periodicity%s" %k] = []
+        for i in range (len(dicts_impr)-1):
+            l = {}
+            if dicts_impr[i] == "'periodicity%s"%k+"':":
+                d={}
+                l= int(dicts_impr[i+1].replace("'," , " ").replace("," , " ").replace("}" , " ").replace("'" , " "))
+                dct["periodicity"+str(k)].append(l)
+        print ("The periodicity%s "%k+"improper list is:")
+        print (dct["periodicity"+str(k)])
+
+
+
+    dct = {}
+    for k in range(1,5):
+        dct["phase%s" %k] = []
+        for i in range (len(dicts_impr)-1):
+            l = {}
+            if dicts_impr[i] == "'phase%s"%k+"':":
+                d={}
+                l= float(dicts_impr[i+1].replace("'," , " ").replace("," , " ").replace("}" , " ").replace("'" , " ").replace("]", " "))
+                dct["phase"+str(k)].append(l)
+        print ("The phase%s "%k+"improper list is:")
+        print (dct["phase"+str(k)])
+
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~ TAG NAME: VIRTUAL SITES ~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    itemlist_VirtualSite = xmldoc.getElementsByTagName('VirtualSite')
+    dicts_virtualsite = []
+    for items in itemlist_VirtualSite:
+        d = {}
+        for a in items.attributes.values():
+            d[a.name] = a.value
+        dicts_virtualsite.append(d)
+    dicts_vs =  str(dicts_virtualsite).split()
+    #print (dicts_vs)
+
+
+    type_vs_List =[]
+    for i in range (len(dicts_vs)-1):
+        t = {}
+        if dicts_vs[i-1] == "[{'type':":
+            t= dicts_vs[i].replace("'," , " ").replace("," , " ").replace("}" , " ").replace("'" , " ")
+            type_vs_List.append(t)
+    print ("The type list for virtual sites is:")
+    print (type_vs_List)
+
+
+    index_vs_List =[]
+    for i in range (len(dicts_vs)-1):
+        t = {}
+        if dicts_vs[i-1] == "'index':":
+            t= dicts_vs[i].replace("'," , " ").replace("," , " ").replace("}" , " ").replace("'" , " ")
+            index_vs_List.append(t)
+    print ("The index list for virtual sites is:")
+    print (index_vs_List)
+
+
+
+    dct = {}
+    for k in range(1,4):
+        dct["atom%s" %k] = []
+        for i in range (len(dicts_vs)-1):
+            l = {}
+            if dicts_vs[i] == "'atom%s"%k+"':":
+                d={}
+                l= int(dicts_vs[i+1].replace("'," , " ").replace("," , " ").replace("}" , " ").replace("'" , " "))
+                dct["atom"+str(k)].append(l)
+        print ("The atom%s "%k+" list for the virtual sites is:")
+        print (dct["atom"+str(k)])
+
+
+
+    dct = {}
+    for k in range(1,3):
+        dct["wo%s" %k] = []
+        for i in range (len(dicts_vs)-1):
+            l = {}
+            if dicts_vs[i] == "'wo%s"%k+"':":
+                d={}
+                l= float(dicts_vs[i+1].replace("'," , " ").replace("," , " ").replace("}" , " ").replace("'" , " "))
+                dct["wo"+str(k)].append(l)
+        print ("The wo%s "%k+" list for the virtual sites is:")
+        print (dct["wo"+str(k)])
+
+
+
+    dct = {}
+    for k in range(1,4):
+        dct["wx%s" %k] = []
+        for i in range (len(dicts_vs)-1):
+            l = {}
+            if dicts_vs[i] == "'wx%s"%k+"':":
+                d={}
+                l= float(dicts_vs[i+1].replace("'," , " ").replace("," , " ").replace("}" , " ").replace("'" , " "))
+                dct["wx"+str(k)].append(l)
+        print ("The wx%s "%k+" list for the virtual sites is:")
+        print (dct["wx"+str(k)])
+
+
+    dct = {}
+    for k in range(1,4):
+        dct["wy%s" %k] = []
+        for i in range (len(dicts_vs)-1):
+            l = {}
+            if dicts_vs[i] == "'wy%s"%k+"':":
+                d={}
+                l= float(dicts_vs[i+1].replace("'," , " ").replace("," , " ").replace("}" , " ").replace("'" , " "))
+                dct["wy"+str(k)].append(l)
+        print ("The wy%s "%k+" list for the virtual sites is:")
+        print (dct["wy"+str(k)])
+
+    dct = {}
+    for k in range(1,4):
+        dct["p%s" %k] = []
+        for i in range (len(dicts_vs)-1):
+            l = {}
+            if dicts_vs[i] == "'p%s"%k+"':":
+                d={}
+                l= dicts_vs[i+1].replace("'," , " ").replace("," , " ").replace("}" , " ").replace("'" , " ")
+                dct["p"+str(k)].append(l)
+        print ("The p%s "%k+" list for the virtual sites is:")
+        print (dct["p"+str(k)])
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~ TAG NAME: RESIDUE ~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    itemlist_residue = xmldoc.getElementsByTagName('Residue')
+    dicts_residue = []
+    for items in itemlist_residue:
+        d = {}
+        for a in items.attributes.values():
+            d[a.name] = a.value
+        dicts_residue.append(d)
+    dicts_res =  str(dicts_residue).split()
+#    print (dicts_res)
+
+
+    residue_List =[]
+    for i in range (len(dicts_res)):
+        t = {}
+        if dicts_res[i-1] == "[{'name':":
+            t= dicts_res[i].replace("'," , " ").replace("," , " ").replace("}]" , " ").replace("'" , " ")
+            residue_List.append(t)
+    print ("The residue name is:")
+    print (residue_List)
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~ TAG NAME: NON BONDED FORCE ~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    itemlist_nonbond = xmldoc.getElementsByTagName('NonbondedForce')
+    dicts_nonb = []
+    for items in itemlist_nonbond:
+        d = {}
+        for a in items.attributes.values():
+            d[a.name] = a.value
+        dicts_nonb.append(d)
+    dicts_nb =  str(dicts_nonb).split()
+ #   print (dicts_nb)
+
+
+    coulomb_List =[]
+    for i in range (len(dicts_nb)):
+        t = {}
+        if dicts_nb[i-1] == "[{'coulomb14scale':":
+            t= float(dicts_nb[i].replace("'," , " ").replace("," , " ").replace("}]" , " ").replace("'" , " "))
+            coulomb_List.append(t)
+    print ("The coulomb 1-4 scale is:")
+    print (coulomb_List)
+
+
+    LJ_List =[]
+    for i in range (len(dicts_nb)):
+        t = {}
+        if dicts_nb[i-1] == "'lj14scale':":
+            t= float(dicts_nb[i].replace("'," , " ").replace("," , " ").replace("}]" , " ").replace("'" , " "))
+            LJ_List.append(t)
+    print ("The Lennard Jones 1-4 scale is:")
+    print (LJ_List)
+
+
     # 3) Now we create an Amberparameters object for each molecule
     molnums = molecules.molNums()
 

2ab3ee93b9365dbfe8b9227337bd26eadfd44489
jmichel80 <mail@julienmichel.net>
2019-02-25 13:21:34 +0000

    re commit

--- readxml/readxml.py
+++ readxml/readxml.py
@@ -1,14 +1,252 @@
 #
 #
+#
+#
 from Sire.IO import *
 from Sire.Mol import *
 from Sire.MM import *
 from Sire.System import *
 from Sire.Units import *
 from Sire.CAS import * 
-from Sire.Maths import * 
+from Sire.Maths import *
+from Sire.Base import *
 
 
+def vsiteListToProperty(list):
+    prop = Properties()
+    i = 0
+    for entry in list:
+        for key, value in entry.items():
+            prop.setProperty("%s(%d)" % (key,i), VariantProperty(value))
+        i += 1
+    prop.setProperty("nvirtualsites",VariantProperty(i))
+    return prop
+
+def readXmlParameters(pdbfile, xmlfile):
+    # 1) Read a pdb file describing the system to simulate
+    xmlfile = 'pyridine/MOL_extra.xml'
+    pdbfile = 'pyridine/MOL.pdb'
+    p = PDB2(pdbfile)
+    s = p.toSystem()
+    molecules = s.molecules()
+    print (molecules)
+
+
+     # 2) Now we read the xml file, and store parameters for each molecule
+    import xml.dom.minidom as minidom
+    xmldoc = minidom.parse(xmlfile)
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~ TAG NAME: TYPE ~~~~~~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    itemlist_type = xmldoc.getElementsByTagName('Type')
+    dicts_type = []
+    for items in itemlist_type:
+        d = {}
+        for a in items.attributes.values():
+            d[a.name] = a.value
+        dicts_type.append(d)
+    dicts_tp =  str(dicts_type).split()
+    print (dicts_tp)
+
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~ TAG NAME: ATOM ~~~~~~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    itemlist_atom = xmldoc.getElementsByTagName('Atom')
+    dicts_atom = []
+    for items in itemlist_atom:
+        d = {}
+        for a in items.attributes.values():
+            d[a.name] = a.value
+        dicts_atom.append(d)
+    dicts_at =  str(dicts_atom).split()
+    print (dicts_at)
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~ TAG NAME: BOND ~~~~~~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    itemlist_bond = xmldoc.getElementsByTagName('Bond')
+    dicts_bond = []
+    for items in itemlist_bond:
+        d = {}
+        for a in items.attributes.values():
+            d[a.name] = a.value
+        dicts_bond.append(d)
+    dicts_b =  str(dicts_bond).split()
+    print (dicts_b)
+
+    nbond = itemlist_bond.length
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~ TAG NAME: ANGLE ~~~~~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    itemlist_angle = xmldoc.getElementsByTagName('Angle')
+    dicts_angle = []
+    for items in itemlist_angle:
+        d = {}
+        for a in items.attributes.values():
+            d[a.name] = a.value
+        dicts_angle.append(d)
+    dicts_ang =  str(dicts_angle).split()
+    print (dicts_ang)
+
+    nAngles= itemlist_angle.length
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~ TAG NAME: PROPER ~~~~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    itemlist_proper = xmldoc.getElementsByTagName('Proper')
+    dicts_proper = []
+    for items in itemlist_proper:
+        d = {}
+        for a in items.attributes.values():
+            d[a.name] = a.value
+        dicts_proper.append(d)
+    dicts_pr =  str(dicts_proper).split()
+    print (dicts_pr)
+
+    nProper = itemlist_proper.length
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~ TAG NAME: IMPROPER ~~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    itemlist_improper = xmldoc.getElementsByTagName('Improper')
+    dicts_improper = []
+    for items in itemlist_improper:
+        d = {}
+        for a in items.attributes.values():
+            d[a.name] = a.value
+        dicts_improper.append(d)
+    dicts_impr =  str(dicts_improper).split()
+    print (dicts_impr)
+    nImproper = itemlist_improper.length
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~ TAG NAME: VIRTUAL SITES ~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    itemlist_VirtualSite = xmldoc.getElementsByTagName('VirtualSite')
+    dicts_virtualsite = []
+    for items in itemlist_VirtualSite:
+        d = {}
+        for a in items.attributes.values():
+            d[a.name] = a.value
+        dicts_virtualsite.append(d)
+    dicts_vs =  str(dicts_virtualsite).split()
+    #print (dicts_vs)
+    nVirtualSites = itemlist_VirtualSite.length 
+        
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~ TAG NAME: RESIDUE ~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    itemlist_residue = xmldoc.getElementsByTagName('Residue')
+    dicts_residue = []
+    for items in itemlist_residue:
+        d = {}
+        for a in items.attributes.values():
+            d[a.name] = a.value
+        dicts_residue.append(d)
+    dicts_res =  str(dicts_residue).split()
+    print (dicts_res)
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~ TAG NAME: NON BONDED FORCE ~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    itemlist_nonbond = xmldoc.getElementsByTagName('NonbondedForce')
+    dicts_nonb = []
+    for items in itemlist_nonbond:
+        d = {}
+        for a in items.attributes.values():
+            d[a.name] = a.value
+        dicts_nonb.append(d)
+    dicts_nb =  str(dicts_nonb).split()
+    print (dicts_nb)
+    nNonBonded = itemlist_nonbond.length
+
+    # 3) Now we create an Amberparameters object for each molecule
+    molnums = molecules.molNums()
+
+    newmolecules = Molecules()
+    for molnum in molnums:
+        mol = molecules.at(molnum)
+        print (mol) #Molecule( 1 version 9 : nAtoms() = 11, nResidues() = 1 )
+        # We populate the Amberparameters object with a list of bond, angle, dihedrals
+        # We look up parameters from the contents of the xml file
+        # We also have to set the atomic parameters (q, sigma, epsilon)
+        editmol = mol.edit()
+        mol_params = AmberParameters(editmol) #SireMol::AmberParameters()
+        atoms = editmol.atoms()
+        # We update atom parameters see setAtomParameters in SireIO/amber.cpp l2122 
+        natoms = editmol.nAtoms()
+        print("number of atoms is %s" %natoms)
+
+        vsites = []
+        for atom in atoms: 
+            editatom = editmol.atom(atom.index())
+            i = int(str(atom.number()).split('(')[1].replace(")" , " "))
+            editatom.setProperty("charge", float(dicts_atom[i+natoms+nVirtualSites]['charge']) * mod_electron)
+            editatom.setProperty("mass", float(dicts_type[i-1]['mass']) * g_per_mol) #
+            editatom.setProperty("LJ", LJParameter( float(dicts_atom[i+natoms+nVirtualSites]['sigma']) * angstrom, float(dicts_atom[i+natoms+nVirtualSites]['epsilon']) * kcal_per_mol))
+            editatom.setProperty("ambertype", dicts_atom[i+natoms+nVirtualSites]['type'])
+
+            #IF THE ATOM IS A VIRTUAL SITE MAKE A DICTIONARY OF THE V-SITE PARAMETERS AND ADD TO vsites (see line below)
+            vsitedict = {"type": "localCoords", "index" : 11, "atom1" : 3}
+            vsites.append(vsitedict)
+            
+            editmol = editatom.molecule()
+            #print(editmol)
+            #print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+        editmol.setProperty("virtual-sites", vsiteListToProperty(vsites))
+
+        # Now we create a connectivity see setConnectivity in SireIO/amber.cpp l2144
+        if natoms > 1:
+            print("Set up connectivity")
+            c = []
+            for atom in atoms:
+                i = int(str(atom.number()).split('(')[1].replace(")" , " "))
+                connect_prop= {}
+                connect_prop = dicts_bond[i-1]['from'], dicts_bond[i-1]['to']
+                c.append(connect_prop)
+            print(c)
+            
+            conn = Connectivity(editmol.info()).edit()
+            for j in range(0,natoms):
+                conn.connect(atoms[int(c[j][0]) ].index(), atoms[int(c[j][1]) ].index()) 
+            editmol.setProperty("connectivity", conn.commit())
+
+        # Now we add bond parameters to the Sire molecule. We also update amberparameters see SireIO/amber.cpp l2154
+        internalff = InternalFF()
+                        
+        bondfuncs = TwoAtomFunctions(mol)
+        r = internalff.symbols().bond().r()
+
+        for j in range(0,natoms):
+            bf = bondfuncs.set(atoms[int(c[j][0]) ].index(), atoms[int(c[j][1]) ].index(), float(dicts_bond[j+natoms]['k'])* (float(dicts_bond[j+natoms]['length']) - r) **2  )
+            bond_id = BondID(atoms[int(c[j][0])].index(), atoms[int(c[j][1])].index())
+            print(bond_id)
+            for i in range(natoms, nbond):
+                mol_params.add(bond_id, float(dicts_bond[i]['k']), float(dicts_bond[i]['length']) ) 
+                        
+            #mol_params.getAllBonds() 
+            #print(mol_params.getAllBonds() )
+        editmol.setProperty("bonds", bondfuncs)
+
+
+        
+        editmol.setProperty("amberparameters", mol_params) # Weird, should work - investigate ? 
+        molecule = editmol.commit()
+        newmolecules.add(molecule)
+        
+    return newmolecules
 
 def xmlParameters(pdbfile, xmlfile):
     # 1) Read a pdb file describing the system to simulate
@@ -168,7 +406,7 @@ def xmlParameters(pdbfile, xmlfile):
     newmolecules = Molecules()
     for molnum in molnums:
         mol = molecules.at(molnum)
-        print (mol) 
+        print (mol) #Molecule( 1 version 9 : nAtoms() = 11, nResidues() = 1 )
         mol_params = AmberParameters() #SireMol::AmberParameters()
         
         # We populate the Amberparameters object with a list of bond, angle, dihedrals
@@ -184,7 +422,7 @@ def xmlParameters(pdbfile, xmlfile):
     #natoms don't include the virtual sites! 
 
     # Loop over each molecule in the molecules object
-    if molecules.count() > 0: 
+    if molecules.count() > 1: 
         moleculeNumbers = molecules.molNums()
         moleculeList = []
 
@@ -210,14 +448,15 @@ def xmlParameters(pdbfile, xmlfile):
 
             # Now we create a connectivity see setConnectivity in SireIO/amber.cpp l2144
             # XML data tells us how atoms are bonded in the molecule (Bond 'from' and 'to')
-            
+            #bondfuncs = k_bondList
+            #new_bond = editmol.setProperty("bonds_new", bondfuncs).commit()
             if natoms > 1:
                 print("Set up connectivity")
 
                 c = []
                 for atom in atoms:
                     i = int(str(atom.number()).split('(')[1].replace(")" , " "))
-                    if natoms > 1:   
+                    if natoms > 1: #if numebr of atoms >1  
                         connect_prop= {}
                         connect_prop = dicts_bond[i-1]['from'], dicts_bond[i-1]['to']
                     c.append(connect_prop)
@@ -455,7 +694,7 @@ def xmlParameters(pdbfile, xmlfile):
         new_file.write("\n")
         new_file.write("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
         new_file.write("\n")
-        new_file.write("The bonds connect the following atoms:\n")
+        new_file.write("The bonds connct the following atoms:\n")
         new_file.write("{0}\n".format(str(mol_params.getAllBonds()).replace(") ),", ") ),\n")))
         new_file.write("\n")
         new_file.write("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
@@ -477,6 +716,7 @@ def xmlParameters(pdbfile, xmlfile):
 
 
 if __name__ == '__main__':
-    xmlParameters('pyridine/MOL.pdb', 'pyridine/MOL_extra.xml')
-
-
+    molecules = readXmlParameters('pyridine/MOL.pdb', 'pyridine/MOL_extra.xml')
+    m0 = molecules.first().molecule()
+    print (m0.properties())
+    editmol = m0.edit()
